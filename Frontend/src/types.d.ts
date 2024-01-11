b5e86d50883f1b89f92126a5067efff3b5ef7778
ElaxDev <elax27@hotmail.com>
2024-01-10 20:50:54 -0400

    Added all returned user properties

--- Frontend/src/types.d.ts
+++ Frontend/src/types.d.ts
@@ -1,13 +1,18 @@
 interface userDataInterface {
+  id: Types.ObjectId;
   uuid: string;
-  avatar: string;
+  avatar: string | undefined;
   username: string;
-  createdAt: number;
-  updatedAt: number;
+  stats: Types.ObjectId | undefined;
+  roles: string[];
+  createdAt: Date | undefined;
+  updatedAt: Date | undefined;
+}
 }
 
 export interface userInterface {
   userData: userDataInterface | null;
+  accessToken: string;
   newUser: boolean;
 }
 
