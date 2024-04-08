16e694c4bb55cab024e02833f2d8e9737410d42c
ElaxDev <elax27@hotmail.com>
2024-04-08 16:45:03 -0400

    Now it calculates level instead of required XP

--- Backend/src/services/calculateLevel.ts
+++ Backend/src/services/calculateLevel.ts
@@ -1,6 +1,6 @@
-export function calculateLevel(level: number) {
+export function calculateLevel(xp: number) {
   const xpVar = 0.07; // This variable controls the amount of xp required per level
   const xpDiff = 1.75; // This variable controls how fast the difference between levels grow
 
-  return Math.floor(Math.pow(level / xpVar, xpDiff));
+  return Math.floor(Math.pow(xp, 1 / xpDiff) * xpVar);
 }
